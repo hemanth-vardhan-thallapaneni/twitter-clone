e5031151901db7dd8313ce71c13ed8ed34afacc0
Hemanth Thallapaneni <tallapaneni.hemanth@gmail.com>
2023-04-01 00:19:05 -0400

    vairable name fix.

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -6,7 +6,7 @@ const {ObjectId} = require('mongodb');
 
 
 const s3 = new Aws.S3({
-    accessKeyId:process.env.AWS_ACCESS_KEY_ID,              // accessKeyId that is stored in .env file
+    accessKeyId:process.env.AWS_ACCESS_KEY,              // accessKeyId that is stored in .env file
     secretAccessKey:process.env.AWS_ACCESS_KEY_SECRET       // secretAccessKey is also store in .env file
 })
 

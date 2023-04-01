126666c4e981a7a18954034be0af86756e40ac73
Hemanth Thallapaneni <tallapaneni.hemanth@gmail.com>
2023-04-01 09:15:23 -0400

    netlify config changes

---
+++ server/functions/api.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routers/routes');
+const authRoute = require('./routers/authRoute')
+
+require("dotenv").config()
+
+
+const corsOptions ={
+    origin:'http://localhost:4200', 
+    credentials:true, 
+    optionSuccessStatus:200
+}
+const app = express();
+
+
+mongoose.connect(process.env.DATABASE_URL,{useUnifiedTopology:true})
+.then(()=>{
+    app.use(cors(corsOptions));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use('/auth',authRoute)
+    app.use('/api',routes);
+
+}).catch(error=>{console.log('Error Occured while connecting to database : ',error)});
+
+const db = mongoose.connection;
+
+db.once("open",()=>{
+    console.log('Connected Successfully!')
+});
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`listening to the port ${process.env.PORT}`)
+});
+
+
+
+
+
+
+
